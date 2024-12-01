15c1c6a635e49e093757fe16eb194080c42f628d
felixmattsson <felix_mattsson@hotmail.com>
2024-12-01 22:31:38 +0100

    task 2

--- 1/1.py
+++ 1/1.py
@@ -13,4 +13,21 @@ with open('input.txt') as f:
     b = np.array(b)
     c = abs(a-b)
     ans = sum(c)
-    print(ans)
+    print("ans to first: ", ans)
+
+# Second part
+# print(a.value_counts())
+import collections
+unique, counts = np.unique(a, return_counts=True)
+a_dict = dict(zip(unique, counts))
+print(a_dict)
+unique, counts = np.unique(b, return_counts=True)
+b_dict = dict(zip(unique, counts))
+# print(b_dict)
+d = []
+for element in a:
+    print(element)
+    d.append(element*np.count_nonzero(b==element))
+d=np.array(d)
+ans_2 = np.sum(d)
+print(ans_2)
